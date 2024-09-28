714095faa13ce259befe3706a4077a1d61fb93c0
Anderson Menezes <andersonmenezes.dev@gmail.com>
2024-09-28 04:04:42 -0300

    chore: remove unused imports and redundant eslint config
    Removed the unused Header component import in app/_layout.tsx to clean up the code. Simplified eslint.config.js by eliminating unnecessary environment settings to streamline configuration.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import {
   initialWindowMetrics,
   SafeAreaProvider,
 } from 'react-native-safe-area-context'
-import Header from '@/components/Header'
 
 export default function RootLayout() {
   return (
