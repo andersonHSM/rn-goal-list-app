117d503d39e5e26530e12f4decbc5a837fe917bf
Anderson Menezes <andersonmenezes.dev@gmail.com>
2024-09-29 09:38:29 -0300

    style: format code for consistency and readability
    Applied consistent formatting across multiple files to enhance code readability and maintain a uniform style. This includes changes such as indentation, line breaks, and spacing in several components, hooks, and constants.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,12 @@
-import { Stack } from 'expo-router'
-import {
-  initialWindowMetrics,
-  SafeAreaProvider,
-} from 'react-native-safe-area-context'
+import { Stack } from 'expo-router';
+import { initialWindowMetrics, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  return (
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-      </Stack>
-    </SafeAreaProvider>
-  )
+	return (
+		<SafeAreaProvider initialMetrics={initialWindowMetrics}>
+			<Stack screenOptions={{ headerShown: false }}>
+				<Stack.Screen name='index' />
+			</Stack>
+		</SafeAreaProvider>
+	);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,27 @@
-import { Text, View } from 'react-native'
-import Header from '@/components/Header'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useThemeColor } from '@/hooks/useThemeColor'
+import { Text, View } from 'react-native';
+import Header from '@/components/Header';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 export default function Page() {
-  const backgroundColor = useThemeColor({}, 'background')
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        s<Header title={'Test'} />
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            shadowRadius: 1,
-            backgroundColor,
-          }}
-        >
-          <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
-        {/*</ScrollView>*/}
-      </SafeAreaView>
-    </>
-  )
+	const backgroundColor = useThemeColor({}, 'background');
+	return (
+		<>
+			<SafeAreaView style={{ flex: 1 }}>
+				s<Header title={'Test'} />
+				<View
+					style={{
+						flex: 1,
+						justifyContent: 'center',
+						alignItems: 'center',
+						shadowRadius: 1,
+						backgroundColor,
+					}}
+				>
+					<Text>Edit app/index.tsx to edit this screen.</Text>
+				</View>
+				{/*</ScrollView>*/}
+			</SafeAreaView>
+		</>
+	);
 }
